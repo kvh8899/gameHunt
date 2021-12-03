c73bfa271c677f21eba8f2b2b6cc834ff415affb
Kyle <kylevhuang@gmail.com>
2021-12-03 15:55:40 -0800

    phase one complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,8 +5,20 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
       const { credential, password } = req.body;
   
