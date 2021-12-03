ead17811c3ac829185b99ad545c9f0c4144c815a
Kyle <kylevhuang@gmail.com>
2021-12-03 15:35:51 -0800

    phase 1

---
+++ backend/routes/api/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const apiRouter = express.Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js');
+
+
+
+/*
+ --- API TESTING CODE ---
+apiRouter.post('/test',function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+
+
+apiRouter.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+    console.log(user);
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+
+apiRouter.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+*/
+module.exports = apiRouter;
---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+const apiRouter = require('./api');
+const express = require('express');
+const router = express.Router();
+
+router.use('/api', apiRouter);
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+module.exports = router;
