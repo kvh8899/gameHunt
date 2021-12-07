03ec4f3297f7a63611b8fbae661c87c434cb0152
Kyle <kylevhuang@gmail.com>
2021-12-07 15:54:16 -0800

    database migrations and seeds

---
+++ backend/db/models/comment.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    postId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    content: DataTypes.TEXT
+  }, {});
+  Comment.associate = function(models) {
+    Comment.belongsTo(models.User,{foreignKey:"userId"})
+    Comment.belongsTo(models.User,{foreignKey:"postId"})
+  };
+  return Comment;
+};
