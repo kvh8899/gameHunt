f7621a41febb395c821ea89adafd2ab265c03f2f
Kyle <kylevhuang@gmail.com>
2021-12-06 17:03:45 -0800

    form functionality

--- frontend/src/components/CreatePost/index.js
+++ frontend/src/components/CreatePost/index.js
@@ -1,26 +1,90 @@
 import "./CreatePost.css";
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
 function CreatePost() {
-    const hist = useHistory();
+  const hist = useHistory();
+  const [title, setTitle] = useState("");
+  const [subHeader, setSubHeader] = useState("");
+  const [headerImage, setHeaderImage] = useState("");
+  const [contentImage, setContentImage] = useState("");
+  const [description, setDescription] = useState("");
   return (
-    <div>
-      <form onSubmit={(e) => {
+    <div className="postCreate">
+      <p>Show off your game to the world!</p>
+      <form
+        className="postCreateForm"
+        onSubmit={async (e) => {
           e.preventDefault();
+          let obj ={
+              header:title,
+              subHeader,
+              headerImage,
+              contentImage,
+              description
+          }
+          const create = await fetch("/api/post", {
+            method: "POST",
+            headers:{'Content-Type':"application/json"},
+            body: JSON.stringify(obj),
+          });
           hist.push("/");
           //set the post that is showing to the one that is created here
-      }}>
+        }}
+      >
         <label htmlFor="header">Title</label>
-        <input type="text" name="header" id="header" required></input>
+        <input
+          type="text"
+          name="header"
+          id="header"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          required
+        ></input>
         <label htmlFor="subHeader">Sub-Header</label>
-        <input type="text" name="subHeader" id="subHeader" required></input>
+        <input
+          type="text"
+          name="subHeader"
+          id="subHeader"
+          value={subHeader}
+          onChange={(e) => {
+            setSubHeader(e.target.value);
+          }}
+          required
+        ></input>
         <label htmlFor="headerImage">Cover Image</label>
-        <input type="text" name="headerImage" id="headerImage"></input>
+        <input
+          type="text"
+          name="headerImage"
+          id="headerImage"
+          value={headerImage}
+          onChange={(e) => {
+            setHeaderImage(e.target.value);
+          }}
+        ></input>
         <label htmlFor="contentImage">Main Image</label>
-        <input type="text" name="contentImage" id="contentImage"></input>
+        <input
+          type="text"
+          name="contentImage"
+          id="contentImage"
+          value={contentImage}
+          onChange={(e) => {
+            setContentImage(e.target.value);
+          }}
+        ></input>
         <label htmlFor="description">Description</label>
-        <input type="text" name="description" id="description"></input>
+        <textarea
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        ></textarea>
+        <button>Create Post</button>
       </form>
-      <button>Create Post</button>
     </div>
   );
 }
