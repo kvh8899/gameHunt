db88346d7e5522b346e503405467aaffefd9c02e
Kyle <kylevhuang@gmail.com>
2021-12-09 14:38:43 -0800

    frontend searchbar

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import LoginFormPage from "../loginForm";
 import SignupFormPage from "../signUpForm";
 import PostProfile from "../PostProfile";
+import SearchContent from "../SearchContent";
 import "./home.css";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -8,20 +9,25 @@ import { useSelector, useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 import { toggle } from "../../store/postshow";
 import { getSinglePost } from "../../store/postProfile";
+import {useRef} from 'react'
 function HomePage() {
   const [hidden, setHidden] = useState(true);
   const [suHidden, setSuHidden] = useState(true);
+  const [searchHide, setSearchHide] = useState(true);
+  const [search, setSearch] = useState("");
+  const [searchContentHidden,setSearchContentHidden] = useState(true);
   const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
   const history = useHistory();
   const { id } = useParams();
+  const bar = useRef(null);
   useEffect(() => {
     if (id) {
       dispatch(toggle(id));
       dispatch(getSinglePost(id));
       history.push(`/posts/${id}`);
     }
-  }, [id,dispatch,history]);
+  }, [id, dispatch, history]);
   return (
     <div
       className="body"
@@ -38,12 +44,38 @@ function HomePage() {
             <Link to="/">
               <img className="logo" src="/gameHunt.png" alt="logo"></img>
             </Link>
-            <Link to="/">About</Link>
-            <input placeholder="Search for games"></input>
+            <div className="searchBar" ref={bar}>
+              <input
+                placeholder="Search for games"
+                value={search}
+                onFocus={(e) => {
+                  setSearchHide(false);
+                }}
+                onBlur={(e) => {
+                  setSearchHide(true);
+                }}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                  if(e.target.value){
+                    setSearchContentHidden(false);
+                    bar.current.style.top="51px";
+                  }else{
+                    setSearchContentHidden(true);
+                    bar.current.style.top="0px";
+                  }
+                }}
+              ></input>
+            {!searchContentHidden?<SearchContent/>: ""}
+            </div>
+            {searchHide ? <Link to="/">About</Link> : ""}
             {sessionUser ? (
-              <Link to="/" className="name">
-                Welcome {sessionUser.username}!
-              </Link>
+              searchHide ? (
+                <Link to="/" className="name">
+                  Welcome {sessionUser.username}!
+                </Link>
+              ) : (
+                ""
+              )
             ) : (
               ""
             )}
@@ -102,7 +134,7 @@ function HomePage() {
       </nav>
       <LoginFormPage hidden={{ hidden, setHidden }} />
       <SignupFormPage suHidden={{ suHidden, setSuHidden }} />
-      <PostProfile suHidden={{ suHidden, setSuHidden }}/>
+      <PostProfile suHidden={{ suHidden, setSuHidden }} />
     </div>
   );
 }
---
+++ frontend/src/components/SearchContent/index.js
@@ -0,0 +1,12 @@
+import './SearchContent.css'
+
+function SearchContent({width}){
+    return (
+        <div className="searchWrapper" style={{width:width?.current?.offsetWidth}}>
+
+        </div>)
+
+}
+
+
+export default SearchContent;
