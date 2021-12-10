15793bb892ad37bcfc0a1e7da2ecd4fd013e3ac0
Kyle <kylevhuang@gmail.com>
2021-12-10 12:17:22 -0800

    styling

--- backend/db/models/post.js
+++ backend/db/models/post.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
   Post.associate = function(models) {
     // associations can be defined here
     Post.belongsTo(models.User,{foreignKey:"userId"})
-    Post.hasMany(models.Comment,{foreignKey:"postId"})
+    Post.hasMany(models.Comment,{foreignKey:"postId",onDelete: 'cascade', hooks: true})
   };
   return Post;
 };
