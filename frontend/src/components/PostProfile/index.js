7385b4cee9498878532ecf749fc8a848e63f6786
Kyle <kylevhuang@gmail.com>
2021-12-10 10:31:49 -0800

    implemented loading animation

--- frontend/src/components/PostProfile/index.js
+++ frontend/src/components/PostProfile/index.js
@@ -34,7 +34,7 @@ function PostProfile({ suHidden }) {
         onClick={(e) => {
           /* reset state */
           dispatch(toggle(null));
-          dispatch(getSinglePost(null))
+          dispatch(getSinglePost(null));
         }}
       ></div>
       <button
@@ -42,11 +42,12 @@ function PostProfile({ suHidden }) {
         onClick={(e) => {
           /* reset state */
           dispatch(toggle(null));
-          dispatch(getSinglePost(null))
+          dispatch(getSinglePost(null));
         }}
       >
         X
       </button>
+
       <div
         className="profileContent"
         onClick={(e) => {
@@ -62,122 +63,134 @@ function PostProfile({ suHidden }) {
           });
         }}
       >
-        <div className="profileHeader">
-          <img
-            className="listImg"
-            src={postProfileData.headerImage}
-            alt="404 not found"
-          ></img>
-          <div className="profHeadings">
-            <h2>{postProfileData.header}</h2>
-            <h3>{postProfileData.subHeader}</h3>
-          </div>
-        </div>
-        <MainContent suHidden={suHidden}/>
-        <CommentForm suHidden={suHidden} />
-        <div className="ss"></div>
-        <div className="commentsContainer">
-          <p>Comments {`(${postComments.length})`}</p>
-          {postComments.map((e, i) => {
-            return (
-              <div key={e.id} className="comment">
-                <div>
-                  <p>{e.User.username}</p>
-                  <p className="cContent" ref={(el) => (pRef.current[i] = el)}>
-                    {e.content}
-                  </p>
-                  <form
-                    className="hidden"
-                    ref={(el) => (editRef.current[i] = el)}
-                    onSubmit={async (submit) => {
-                      submit.preventDefault();
-                      let obj = {
-                        userId: sessionUser.id,
-                        postId: postProfileData[0]?.id,
-                        content: edit,
-                        createdAt: e.createdAt,
-                      };
-                      await dispatch(
-                        updateComment(obj, e.id, {
-                          username: sessionUser.username,
-                          id: sessionUser.id,
-                          email: sessionUser.email,
-                        })
-                      );
-                      editRef.current[i].classList.toggle("hidden");
-                      pRef.current[i].classList.toggle("hidden");
-                    }}
-                  >
-                    <input
-                      value={edit}
-                      onClick={(event) => {
-                        event.stopPropagation();
-                      }}
-                      onChange={(e) => {
-                        setEdit(e.target.value);
-                      }}
-                    ></input>
-                  </form>
-                </div>
-                {e.userId === sessionUser?.id ? (
-                  <div className="menu">
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        /* this closes other comments modals*/
-                        commRef.current.forEach((event) => {
-                          if (event && event.id !== commRef.current[i].id) {
-                            event.classList.add("hidden");
-                          }
-                        });
-                        editRef.current.forEach((event) => {
-                          if (event && event.id !== commRef.current[i].id) {
-                            event.classList.add("hidden");
-                          }
-                        });
-                        pRef.current.forEach((event) => {
-                          if (event && event.id !== commRef.current[i].id) {
-                            event.classList.remove("hidden");
-                          }
-                        });
-                        commRef.current[i].classList.toggle("hidden");
-                      }}
-                    >
-                      <i className="fa fa-ellipsis-h"></i>
-                    </button>
-                    <div
-                      ref={(el) => (commRef.current[i] = el)}
-                      className="editComment hidden"
-                      id={e.id}
-                    >
-                      <button
-                        /* close modals when clicking the button*/
-                        onClick={(event) => {
-                          event.stopPropagation();
-                          commRef.current[i].classList.toggle("hidden");
+        {postProfileData.id ? (
+          <>
+            {" "}
+            <div className="profileHeader">
+              <img
+                className="listImg"
+                src={postProfileData.headerImage}
+                alt="404 not found"
+              ></img>
+              <div className="profHeadings">
+                <h2>{postProfileData.header}</h2>
+                <h3>{postProfileData.subHeader}</h3>
+              </div>
+            </div>
+            <MainContent suHidden={suHidden} />
+            <CommentForm suHidden={suHidden} />
+            <div className="ss"></div>
+            <div className="commentsContainer">
+              <p>Comments {`(${postComments.length})`}</p>
+              {postComments.map((e, i) => {
+                return (
+                  <div key={e.id} className="comment">
+                    <div>
+                      <p>{e.User.username}</p>
+                      <p
+                        className="cContent"
+                        ref={(el) => (pRef.current[i] = el)}
+                      >
+                        {e.content}
+                      </p>
+                      <form
+                        className="hidden"
+                        ref={(el) => (editRef.current[i] = el)}
+                        onSubmit={async (submit) => {
+                          submit.preventDefault();
+                          let obj = {
+                            userId: sessionUser.id,
+                            postId: postProfileData[0]?.id,
+                            content: edit,
+                            createdAt: e.createdAt,
+                          };
+                          await dispatch(
+                            updateComment(obj, e.id, {
+                              username: sessionUser.username,
+                              id: sessionUser.id,
+                              email: sessionUser.email,
+                            })
+                          );
                           editRef.current[i].classList.toggle("hidden");
                           pRef.current[i].classList.toggle("hidden");
-                          setEdit(e.content);
                         }}
                       >
-                        Edit
-                      </button>
-                      <button
-                        onClick={(event) => {
-                          dispatch(deleteComment(e.id));
-                        }}
-                      >
-                        Delete
-                      </button>
+                        <input
+                          value={edit}
+                          onClick={(event) => {
+                            event.stopPropagation();
+                          }}
+                          onChange={(e) => {
+                            setEdit(e.target.value);
+                          }}
+                        ></input>
+                      </form>
                     </div>
+                    {e.userId === sessionUser?.id ? (
+                      <div className="menu">
+                        <button
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            /* this closes other comments modals*/
+                            commRef.current.forEach((event) => {
+                              if (event && event.id !== commRef.current[i].id) {
+                                event.classList.add("hidden");
+                              }
+                            });
+                            editRef.current.forEach((event) => {
+                              if (event && event.id !== commRef.current[i].id) {
+                                event.classList.add("hidden");
+                              }
+                            });
+                            pRef.current.forEach((event) => {
+                              if (event && event.id !== commRef.current[i].id) {
+                                event.classList.remove("hidden");
+                              }
+                            });
+                            commRef.current[i].classList.toggle("hidden");
+                          }}
+                        >
+                          <i className="fa fa-ellipsis-h"></i>
+                        </button>
+                        <div
+                          ref={(el) => (commRef.current[i] = el)}
+                          className="editComment hidden"
+                          id={e.id}
+                        >
+                          <button
+                            /* close modals when clicking the button*/
+                            onClick={(event) => {
+                              event.stopPropagation();
+                              commRef.current[i].classList.toggle("hidden");
+                              editRef.current[i].classList.toggle("hidden");
+                              pRef.current[i].classList.toggle("hidden");
+                              setEdit(e.content);
+                            }}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            onClick={(event) => {
+                              dispatch(deleteComment(e.id));
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <div className="fa-3x">
+            <i className="fa fa-spinner fa-pulse"></i>
+          </div>
+        )}
       </div>
     </div>
   ) : (
